8dba9ceb48faf8326611e599a5ff78371771a8f0
Rana Ahsan Ansar <asn.cs21@gmail.com>
2022-06-06 22:12:58 -0700

    Show new User Message

--- js/client.js
+++ js/client.js
@@ -4,9 +4,23 @@ const form = document.getElementById('msg_form');
 
 const messageInput = document.getElementById('msginput');
 
-const messageContainer = document.querySelector(".inbox_container");
+const messageContainer = document.querySelector(".inbox");
+
+const append = (message, position)=>{
+    const mesaageElement = document.createElement('div');
+    mesaageElement.innerText = message;
+    mesaageElement.classList.add('message');
+    mesaageElement.classList.add(position);
+
+    messageContainer.append(mesaageElement);
+}
+
 
 const username = prompt("Enter your name:");
 
-
 socket.emit('new-user-joined' , username);
+
+
+socket.on('user-joined' , data=>{
+    append(`${username} joined the Chat` , 'left');
+});
